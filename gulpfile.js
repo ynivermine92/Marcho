bc8b983c670b56e163adb2420c11734482024359
ynivermine92 <ynivermine92@gmail.com>
2022-06-03 17:34:09 +0300

    sectipn slider active

--- gulpfile.js
+++ gulpfile.js
@@ -42,6 +42,7 @@ const imageMin = () => {
 const scripts = () =>{
 	return src([
 		'node_modules/jquery/dist/jquery.js',
+		'node_modules/slick-carousel/slick/slick.js',
 		'app/js/main.js'
 	])
 	.pipe(concat('main.min.js'))
