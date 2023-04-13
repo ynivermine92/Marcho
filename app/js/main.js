6b9b9ac8555d128f85d0ca27ebc55aec551d5ad9
unknown <ynivermine92@gmail.com>
2023-04-13 20:27:43 +0300

    adaptiv product

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,54 @@
 $(function(){
 
+  document.querySelectorAll(".select").forEach((select) => {
+    const selectBtn = select.querySelector(".select__btn");
+    const selectList = select.querySelector(".select__list");
+    const  selectLinks = select.querySelectorAll(".select__link");
+  
+    selectBtn.addEventListener("click", () => {
+      selectBtn.classList.toggle("active");
+      selectList.classList.toggle("active");
+  
+      if (selectList.style.maxHeight) {
+        selectList.style.maxHeight = null;
+        selectList.removeAttribute('style');
+      } else {
+        selectList.style.maxHeight = selectList.scrollHeight + "px";
+      }
+    });
+  
+    selectLinks.forEach((selectLink) => {
+      selectLink.addEventListener('click', selectActive)
+    });
+  
+    function selectActive(e) {
+      selectLinks.forEach((selectLink) => {
+        selectLink.classList.remove('active');
+  
+        if (e.target.closest("a")) {
+          selectBtn.innerHTML = "";
+          selectBtn.append(e.target.cloneNode(true));
+  
+          selectHide();
+        }
+      });
+      this.classList.add('active');
+    }
+  
+    let selectHide = () => {
+      selectBtn.classList.remove("active");
+      selectList.classList.remove("active");
+      selectList.removeAttribute('style');
+    };
+    document.addEventListener("mouseup", (e) => {
+      if (!select.contains(e.target)) selectHide();
+    });
+  });
+
+
+
+
+
   let menu = document.querySelector('.burger-menu');
     menu.addEventListener('click',(e)=>{
         document.body.style.overflow ='hidden'
